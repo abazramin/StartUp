33ba0bb2bd70d583090ee1ca20848b65b4f85e5c
Abazr Amin Hamza <azr032024@gmail.com>
2025-05-18 15:07:26 +0300

    add connection string

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+//	options.UseSqlServer(builder.Configuration.GetConnectionString("cs"))
 //    );
 
 // builder.Services.AddRazorPages();
